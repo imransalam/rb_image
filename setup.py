84d1a3d8f5c9ddeee08a14c9cd02441d1754a2b5
imransalam <imran.salam.24@gmail.com>
2019-03-14 13:22:03 +0500

    Added Docgen

--- setup.py
+++ setup.py
@@ -1,24 +1,16 @@
-from setuptools import setup, find_packages
-import sys, os
+from setuptools import setup
+
 
 version = '0.1'
 
 setup(name='rb_image',
       version=version,
       description="Scikit-image wrapper for Red Buffer. Contains all the utility functions.",
-      long_description="""Scikit-image wrapper""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='rb skimage',
-      author='imransalam',
+      long_description=open('README.md').read(),
+      author='Imran us Salam, Mian',
       author_email='imran.salam.24@gmail.com',
       license='MIT',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      packages=['rb_image'],
       include_package_data=True,
-      zip_safe=True,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
+      install_requires=['scikit-image'],
       )
